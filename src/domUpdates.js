6c9c5c26dacf293c2ed79101ca7acfb0a1d32b5d
Kristen Bair <kristenmbair@gmail.com>
2021-01-15 22:53:20 -0700

    Check instatiations on DOM display

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,5 +1,21 @@
 const domUpdates = {
+  displayTrips(currentUser, allTrips) {
+    const tripCards = document.querySelector('.card-container');
+    let tripInfo = '';
+    console.log(currentUser)
+    allTrips.forEach(trip => {
+      tripInfo += `
+      <article class="trip-cards">
+      <p>Date: ${trip.date}</p> <br>
+      <p>Duration: ${trip.duration}</p> <br>
+      <p>Status: ${trip.status}</p> <br>
+      <p>Activities: ${trip.suggestedActivities}</p>
+      </article>
 
+      `;
+    })
+    tripCards.insertAdjacentHTML('beforeend', tripInfo)
+  }
 }
 
 export default domUpdates;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,39 @@ import './images/turing-logo.png';
 
 import Traveler from './Traveler.js';
 import Trip from './Trip.js';
+import Destination from './Destination.js';
 import domUpdates from './domUpdates.js';
 import apiCalls from './apiCalls.js'
 
+let allTravelers;
+let allTrips;
+let allDestinations;
+let currentTraveler;
+
 window.addEventListener('load', () => {
-  apiCalls.getAllTravelers().then(travelers => console.log(travelers))
+  apiCalls.fetchAllData()
+    .then(allData => {
+      allTravelers = allData[0];
+      allTrips = allData[1];
+      allDestinations = allData[2];
+      createUser();
+      createTrip();
+      createDestination();
+      domUpdates.displayTrips(currentTraveler, allTrips)
+    })
 })
+
+function createUser() {
+  currentTraveler = new Traveler(allTravelers[0])
+  // console.log(currentTraveler);
+}
+
+function createTrip() {
+  let aTrip = new Trip(allTrips[0]);
+  console.log(aTrip)
+}
+
+function createDestination() {
+  let destination = new Destination(allDestinations[0])
+  console.log(destination);
+}
