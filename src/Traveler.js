d8de98d7fbdb7ae1518361f81ff031588b63d138
Kristen Bair <kristenmbair@gmail.com>
2021-01-15 20:15:39 -0700

    Create properties on traveler class to reflect api data

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,6 +1,13 @@
 class Traveler {
-  constructor() {
-
+  constructor(travelerData) {
+    this.id = travelerData.id;
+    this.name = travelerData.name;
+    this.type = travelerData.travelerType;
+    this.allTrips = [];
+    this.upcoming = [];
+    this.present;
+    this.past = [];
+    this.pending = [];
   }
 }
 
