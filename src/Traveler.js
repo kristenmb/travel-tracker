66ab45a74840df8a184615026e2395ec8346ba30
Kristen Bair <kristenmbair@gmail.com>
2021-01-15 22:51:34 -0700

    Add properties to store traveler info

--- src/Traveler.js
+++ src/Traveler.js
@@ -5,9 +5,32 @@ class Traveler {
     this.type = travelerData.travelerType;
     this.allTrips = [];
     this.upcoming = [];
-    this.present;
+    this.present = [];
     this.past = [];
     this.pending = [];
+  }
+
+  createAllTrips() {
+
+    //sort all trip data to filter out only the trips with matching userID
+    //add destination info
+    //instantiate Trips and push into allTrips array
+  }
+
+  sortPresentTrips() {
+    // filter trips for any occurring today and push to this.present
+  }
+
+  sortUpcomingTrips() {
+    //filter all upcoming trips and push to this.upcoming
+  }
+
+  sortPastTrips() { 
+    //filter all past trips and push to this.past
+  }
+
+  calculateMoneySpentThisYear() {
+    //for each trip annualMoney += ((duration * cost per day) + (number of travelers * flightscost) * 10%)
   }
 }
 
