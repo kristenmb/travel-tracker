382de15d3a514324b51181e6a4c30aefd799a17b
Kristen Bair <kristenmbair@gmail.com>
2021-01-15 20:16:00 -0700

    Create properties to reflect api data for trips

--- src/Trip.js
+++ src/Trip.js
@@ -1,7 +1,16 @@
 class Trip {
-  constructor() {
-
+  constructor(tripData, destinationsData) {
+    this.id = tripData.id;
+    this.userID = tripData.userID;
+    // this.destination = destinationsData.id;
+    this.date = tripData.date;
+    this.duration = tripData.duration;
+    this.status = tripData.status;
+    //or pending?
+    this.activities = tripData.suggestedActivities;
   }
 }
 
+
+//this.date --> maybe want to date = this.date.split('/') and display as date[1], date[2], date[0]
 export default Trip;
