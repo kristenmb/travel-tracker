8d4bc8ae3beabf8da4e590dceaa7d822946cb94e
Kristen Bair <kristenmbair@gmail.com>
2021-01-17 10:57:20 -0700

    Calculate trip cost and yearly money spent on trips

--- src/Trip.js
+++ src/Trip.js
@@ -1,17 +1,10 @@
+import Destination from './Destination.js';
+
 class Trip {
   constructor(tripData, destinationData) {
     this.id = tripData.id;
     this.userID = tripData.userID;
     this.destination = destinationData;
-    //an object of destination info from api
-    //  {
-    //         "id": 1,
-    //         "destination": "Lima, Peru",
-    //         "estimatedLodgingCostPerDay": 70,
-    //         "estimatedFlightCostPerPerson": 400,
-    //         "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
-    //         "alt": "overview of city buildings with a clear sky"
-    //     },
     this.travelerCount = tripData.travelers;
     this.date = tripData.date;
     this.duration = tripData.duration;
@@ -20,13 +13,16 @@ class Trip {
     this.activities = tripData.suggestedActivities;
     this.tripStartDate;
     this.tripEndDate;
+    this.tripCost;
   }
 
   estimatedTripCost() {
-    const totalLodging = this.destination.costPerDay * this.duration;
-    const totalFlight = this.destination.costPerFlight * this.travelerCount;
-    const tripCost = ((totalLodging + totalFlight) * .10).toFixed(2);
-    return tripCost;
+    const totalLodging = this.destination.estimatedLodgingCostPerDay * this.duration;
+    const totalFlight = this.destination.estimatedFlightCostPerPerson * this.travelerCount;
+    const tripCost = totalLodging + totalFlight;
+    const costWithAgentFee = tripCost * .10;
+    this.tripCost = costWithAgentFee;
+    return costWithAgentFee;
   }
 
   findTripDuration() {
